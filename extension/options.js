06a9506b5fb6d476e664d959a97af15e16d595c0
cuamckuu <sasha23969@gmail.com>
2018-07-31 01:09:08 +0300

    Delete button for queries at options page. Deleted extra button class in css.

--- extension/options.js
+++ extension/options.js
@@ -20,16 +20,33 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 */
 
-    const drawQuery = function (query) {
+    const deleteQuery = function (queries, index) {
+        queries.splice(index, 1);
+        localStorage.queries = JSON.stringify(queries);
+
+        location.reload();
+    }
+
+    const drawQuery = function (query, index, arr) {
         let block = document.createElement("div");
         block.className = "note";
+        block.index = index;
 
-        let res = "";
+        let removeBtn = document.createElement("div");
+        removeBtn.className = "button";
+        removeBtn.innerHTML = "X";
+        removeBtn.addEventListener("click", function(event){deleteQuery(arr, index);});
+        block.append(removeBtn);
+
+        let res = document.createDocumentFragment();
         Object.keys(query).forEach((key) => {
-            res += `${key.bold()}: ${query[key]} <br>`;
+            let row = document.createElement("span");
+            row.innerHTML = `${key.bold()}: ${query[key]} <br>`
+
+            res.append(row);
         });
 
-        block.innerHTML = res;
+        block.append(res);
 
         let background = document.getElementsByClassName("background")[0];
         background.append(block);
@@ -59,6 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
         createTextInput(form, "format", "Format");
 
         let submit = document.createElement("input");
+        submit.className = "button";
         submit.type = "submit";
         form.append(submit);
 
@@ -68,11 +86,14 @@ document.addEventListener("DOMContentLoaded", function () {
         return form;
     }
 
-    const saveQuery = function (event) {
+    const saveFromForm = function (event) {
         const form = event.target;
 
         let formData = {};
-        Object.values(form).forEach( (input) => {
+        Object.keys(form).forEach( (index) => {
+            if (+index === form.length - 1) return;
+
+            let input = form[index];
             formData[input.title] = input.value.length === 0?
                                     input.placeholder:
                                     input.value;
@@ -80,14 +101,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
         queries.push(formData);
         localStorage.queries = JSON.stringify(queries);
-    };
+     };
 
     const getQueries = function () {
         return JSON.parse(localStorage.queries);
     }
 
     const initLocalStorage = function () {
-        if (!!localStorage.queries === false) {
+        if (!localStorage.queries) {
             localStorage.queries = "[]";
         }
     }
@@ -98,6 +119,6 @@ document.addEventListener("DOMContentLoaded", function () {
     queries.forEach(drawQuery);
 
     let form = drawInput();
-    form.addEventListener("submit", saveQuery);
+    form.addEventListener("submit", saveFromForm);
 });
 
